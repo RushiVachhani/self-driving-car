f470bf44e594b373e7dd221c84159b35373df42a
Rushi Vachhani <vachhanirushi30@gmail.com>
2024-01-02 10:39:49 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// get the canvas and set the width and the height
+const canvas = document.getElementById("mainCanvas");
+canvas.height = window.innerHeight;
+canvas.width = 200;
+
+// get the canvas context
+const ctx = canvas.getContext("2d");
