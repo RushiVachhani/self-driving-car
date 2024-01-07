84ae8fd9b8ff248db9f2ae6520db64d33565229c
Rushi Vachhani <vachhanirushi30@gmail.com>
2024-01-07 12:10:08 +0530

    add speed physics to car

--- car.js
+++ car.js
@@ -7,16 +7,38 @@ export class Car {
     this.width = width;
     this.height = height;
 
+    this.speed = 0;
+    this.acceleration = 0.2;
+
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+
     this.controls = new Controls();
   }
 
   update() {
     if (this.controls.forward) {
-      this.y = this.y - 2;
+      this.speed = this.speed + this.acceleration;
     }
     if (this.controls.reverse) {
-      this.y = this.y + 2;
+      this.speed = this.speed - this.acceleration;
     }
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < (this.maxSpeed / 2) * -1) {
+      this.speed = (this.maxSpeed / 2) * -1;
+    }
+    if (this.speed > 0) {
+      this.speed = this.speed - this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed = this.speed + this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+    this.y = this.y - this.speed;
   }
 
   draw(ctx) {
